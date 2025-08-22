d9808da9ccc0dee4371763ca49015cdb59b00d22
sakinkhan <sakin2044@gmail.com>
2025-08-23 03:18:39 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Login button functionality
+document
+  .getElementById("loginButton")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const mobileNumber = 0470365288;
+    const pinNumber = 2044;
+    const mobileNumberValue = parseInt(document.getElementById("mobile-number").value);
+    const pinNumberValue = parseInt(document.getElementById("pin-number").value);
+    if (mobileNumberValue === mobileNumber && pinNumberValue === pinNumber) {
+        window.location.href='./home.html';
+    }
+    else {
+        alert("Invalid credentials");
+    }
+  });
